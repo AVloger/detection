e1e2b54ff965f3b33822637390884278fceb5b4b
li7819559 <243637296@qq.com>
2022-12-29 11:38:58 +0800

    update weight

--- Flask/processor/AIDetector_pytorch.py
+++ Flask/processor/AIDetector_pytorch.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 from models.experimental import attempt_load
-from utils.general import non_max_suppression, scale_coords, letterbox
+from utils.general import non_max_suppression, scale_coords
 from utils.torch_utils import select_device
 import cv2
 from random import randint
@@ -17,10 +17,10 @@ class Detector(object):
 
     def init_model(self):
 
-        self.weights = 'weights/best.pt'
+        self.weights = '/Users/lixiang/IdeaProjects/detectionV6/Flask/weights/best.pt'
         self.device = '0' if torch.cuda.is_available() else 'cpu'
         self.device = select_device(self.device)
-        model = attempt_load(self.weights, map_location=self.device)
+        model = attempt_load(self.weights)
         model.to(self.device).eval()
         model.float()
         # torch.save(model, 'test.pt')
@@ -49,17 +49,20 @@ class Detector(object):
         tl = line_thickness or round(
             0.002 * (image.shape[0] + image.shape[1]) / 2) + 1  # line/font thickness
         for (x1, y1, x2, y2, cls_id, conf) in bboxes:
-            color = self.colors[self.names.index(cls_id)]
-            c1, c2 = (x1, y1), (x2, y2)
-            cv2.rectangle(image, c1, c2, color,
-                          thickness=tl, lineType=cv2.LINE_AA)
-            tf = max(tl - 1, 1)  # font thickness
-            t_size = cv2.getTextSize(
-                cls_id, 0, fontScale=tl / 3, thickness=tf)[0]
-            c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-            cv2.rectangle(image, c1, c2, color, -1, cv2.LINE_AA)  # filled
-            cv2.putText(image, '{} ID-{:.2f}'.format(cls_id, conf), (c1[0], c1[1] - 2), 0, tl / 3,
-                        [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+            if cls_id == 'person' or cls_id == 'cap' or cls_id == 'shoes':
+                for i,n in self.names.items():
+                    if n == cls_id:
+                        color = self.colors[i]
+                c1, c2 = (x1, y1), (x2, y2)
+                cv2.rectangle(image, c1, c2, color,
+                            thickness=tl, lineType=cv2.LINE_AA)
+                tf = max(tl - 1, 1)  # font thickness
+                t_size = cv2.getTextSize(
+                    cls_id, 0, fontScale=tl / 3, thickness=tf)[0]
+                c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+                cv2.rectangle(image, c1, c2, color, -1, cv2.LINE_AA)  # filled
+                cv2.putText(image, '{} ID-{:.2f}'.format(cls_id, conf), (c1[0], c1[1] - 2), 0, tl / 3,
+                            [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
         return image
 
     def detect(self, im):
@@ -91,3 +94,38 @@ class Detector(object):
 
         im = self.plot_bboxes(im, pred_boxes)
         return im, image_info
+
+
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True):
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - \
+        new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, 32), np.mod(dh, 32)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / \
+            shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(
+        img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
